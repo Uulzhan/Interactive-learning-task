b83eab06a8994a0106a42109e21d14d74afa5842
Uulzhan <uulka07@gmail.com>
2021-07-28 23:25:03 +0000

    added changes

--- ec2/output.tf
+++ ec2/output.tf
@@ -4,6 +4,7 @@ output "EC2Output" {
         Machine has the following IP address   ${aws_instance.terraform_instance.public_ip}
         Machine has the following ID           ${aws_instance.terraform_instance.id}
         Machine is in                          ${aws_instance.terraform_instance.availability_zone}
+        Machine 
 
         
     EOF
