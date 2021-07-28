998e288e781f5bd615af22593c2c57d8ab86b642
Uulzhan <uulka07@gmail.com>
2021-07-28 23:37:42 +0000

    added changes

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,15 +1,15 @@
 output "AZ"  {
-  value  = aws_instance.terraform_instance.availability_zone
+   value = aws_instance.terraform_instance.availability_zone
 }
 
 output "public_ip" {
-    value = ${aws_instance.terraform_instance.public_ip}
+    value = aws_instance.terraform_instance.public_ip
 }
 
 output "instance_id" {
-    value = ${aws_instance.terraform_instance.id}
+    value = aws_instance.terraform_instance.id
 }
 
 output "region" {
-    value = ${aws_instance.terraform_instance.region}
+    value = aws_instance.terraform_instance.region
 }
