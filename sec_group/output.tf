84f90c8a8f504dbca1818355e90471812fbdbceb
Uulzhan <uulka07@gmail.com>
2021-07-29 00:12:16 +0000

    commit  changes

---
+++ sec_group/output.tf
@@ -0,0 +1,11 @@
+output "sec_group_id" {
+    value = 
+}
+
+output "region" {
+    value = "us-east-1"
+}
+
+output " attched vpc"{
+    value = 
+}
---
+++ sec_group/sec_group.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "interactive-learning" {
+  name        = "interactive-learning"
+  description = "interactive learning tasks"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["50.194.68.230/32"]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["50.194.68.230/32"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
