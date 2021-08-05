8f1160c8e06bd28f783e97df0b2a767523792038
Uulzhan <uulka07@gmail.com>
2021-08-05 05:41:24 +0000

    added iam.tf

--- iam/iam.tf
+++ iam/iam.tf
@@ -1,3 +1,18 @@
-resource " aws_iam_user" "users {
-    
+resource " aws_iam_user" "user"{
+    name = "bob"
+    path = "/system/"
+
+    tags = {
+        Team = "DevOps"
+    }
+
+}
+
+resource "aws_iam_group" "sysusers" {
+    name = "bob"
+    path = "/user/"
+
+    tags = {
+        Team = "DevOps"
+    } 
 }
