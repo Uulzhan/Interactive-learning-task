7b47f1cb4ce2153dacea20b7b25413ff0edc4b9a
Uulzhan <uulka07@gmail.com>
2021-08-05 06:12:16 +0000

    added changes

--- iam/iam.tf
+++ iam/iam.tf
@@ -1,15 +1,22 @@
-resource "aws_iam_user" "user" {
+resource "aws_iam_user" "bob" {
   name = "bob"
-  path = "/system/"
 
   tags = {
     Team = "DevOps"
   }
-
 }
 
 resource "aws_iam_group" "sysusers" {
   name = "sysusers"
-  path = "/users/"
+  
+}
 
+resource "aws_iam_group_membership" "team" {
+    name = "sysusers"
+
+    users = [
+        aws_iam_user.bob.name,
+    ]
+
+    group = aws_iam_group.sysusers.name
 }
